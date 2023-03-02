48d13467ebc8cd8feec55c6615572250d11e73bf
Black Hat Ethical Hacking <chris@blackhatethicalhacking.com>
2023-03-02 15:51:06 +0200

    SSRFPwned.sh V1.0
    SSRFPwned.sh V1.0

--- SSRFPwned.sh
+++ SSRFPwned.sh
@@ -12,9 +12,9 @@ echo "Offensive Security Tip: $random_quote - Sun Tzu" | lolcat
 sleep 1
 echo "MEANS, IT'S ☕ 1337 ⚡ TIME, 369 ☯ " | lolcat
 sleep 1
-figlet -w 80 -f small SRFPwned | lolcat
+figlet -w 80 -f small SSRFPwned | lolcat
 echo ""
-echo "[YOUR ARE USING SRFPwned.sh] - (v1.0) CODED BY Chris 'SaintDruG' Abou-Chabké WITH ❤ FOR blackhatethicalhacking.com for Educational Purposes only!" | lolcat
+echo "[YOUR ARE USING SSRFPwned.sh] - (v1.0) CODED BY Chris 'SaintDruG' Abou-Chabké WITH ❤ FOR blackhatethicalhacking.com for Educational Purposes only!" | lolcat
 sleep 1
 #check if the user is connected to the internet
 tput bold;echo "CHECKING IF YOU ARE CONNECTED TO THE INTERNET!" | lolcat
@@ -34,43 +34,43 @@ output_dir="$domain-$(date +%Y%m%d%H%M%S)"
 mkdir "$output_dir"
 
 # Fetch URLs from domain using waybackurls
-echo "Fetching URLs using Waybackmachine..."
+echo "Fetching URLs using Waybackmachine..." | lolcat
 waybackurls $domain > "$output_dir/waybackurls.txt"
 total=$(wc -l < "$output_dir/waybackurls.txt")
-echo "Found $total URLs from Waybackmachine"
+echo "Found $total URLs from Waybackmachine" | lolcat
 sleep 2
 
 # Fetch URLs from domain using AlienVault
-echo "Fetching URLs using AlienVault..."
+echo "Fetching URLs using AlienVault..." | lolcat
 curl -s "https://otx.alienvault.com/api/v1/indicators/domain/$domain/passive_dns" | jq -r '.passive_dns[].hostname' | sed 's/\*\.//g' | sort -u > "$output_dir/alien_urls.txt"
 total=$(wc -l < "$output_dir/alien_urls.txt")
-echo "Found $total URLs from AlientVault"
+echo "Found $total URLs from AlientVault" | lolcat
 sleep 2
 # Fetch URLs from domain using URLScan
-echo "Fetching URLs using URLScan..."
+echo "Fetching URLs using URLScan..." | lolcat
 curl -s "https://urlscan.io/api/v1/search/?q=domain:$domain" | jq -r '.results[].page.url' | sort -u > "$output_dir/urlscan_urls.txt"
 total=$(wc -l < "$output_dir/urlscan_urls.txt")
-echo "Found $total URLs from URLScan"
+echo "Found $total URLs from URLScan" | lolcat
 sleep 2
 
-echo "Combining all URLs and Filtering out all Irrelevant extensions while cleaning all Parameters..."
+echo "Combining all URLs and Filtering out all Irrelevant extensions while cleaning all Parameters..." | lolcat
 # Combine the two URL lists and filter out irrelevant extensions
 cat "$output_dir/waybackurls.txt" "$output_dir/alien_urls.txt" "$output_dir/urlscan_urls.txt" | grep -E -v '\.(png|jpg|jpeg|gif|pdf|css|js)$' | sort -u > "$output_dir/all_urls.txt"
 sleep 1
 # Test URLs for SSRF
-echo "Extracting Alive URls, Filter them with GF for SSRF..."
+echo "Extracting Alive URls, Filter them with GF for SSRF..." | lolcat
 cat "$output_dir/all_urls.txt" | httpx --verbose | sed 's/=.*/=/' | gf ssrf > "$output_dir/final_urls.txt"
 total=$(wc -l < "$output_dir/final_urls.txt")
-echo "Found $total URLs ideal for SSRF"
+echo "Found $total URLs ideal for SSRF..." | lolcat
 sleep 5
 
 # Read payloads from directory - modify it to add more after the cp command
-echo "Using Internal Payloads..."
+echo "Using Internal SSRF Payloads..." | lolcat
 cp ssrfpayloads.txt "$output_dir/"
 cat "$output_dir/ssrfpayloads.txt" > "$output_dir/merged_payloads.txt"
 
 # Test URLs for SSRF
-echo "Testing URLs for SSRF vulnerabilities..."
+echo "Testing URLs for SSRF vulnerabilities..." | lolcat
 
 # Loop through each URL and test for SSRF
 while read url; do
@@ -102,4 +102,4 @@ done < "$output_dir/final_urls.txt"
 
 # Count the number of vulnerable URLs found and print the total
 total_vulnerable=$(wc -l < "$output_dir/vulnerable.txt")
-echo "Found $total_vulnerable vulnerable URLs."
+echo "Found $total_vulnerable vulnerable URLs." | lolcat
